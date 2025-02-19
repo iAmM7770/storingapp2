4f6332b5ee5af30e578052988c403c4a42e820b4
iAmM_777 <m02403992@gmail.com>
2025-02-19 13:48:55 +0100

    meldingen

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -1,30 +1,69 @@
 <?php
 
+$action = $_POST['action'];
+if($action == 'create') {   
+
+
+
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+if(empty($attractie))
+{
+    $errors[] = "Vul attractie-naam in.";
+}
 $capaciteit = $_POST['capaciteit'];
+if(!is_numeric($capaciteit))
+{
+    $errors[] = "Vul voor capaciteit een geldig getal in.";
+}
 $melder = $_POST['melder'];
+if(empty($melder))
+{
+    $errors[] = "Vul melder-naam in.";
+}
 $type = $_POST['type'];
-
-//echo $attractie . " / " . $capaciteit . " / " . $melder;
+if(empty($type))
+{
+    $errors[] = "Vul type-naam in.";
+}
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = 1;
+}
+else
+{
+    $prioriteit = 0;
+}
+$overig = $_POST['overig'];
+if(isset($errors))
+{
+    var_dump($errors);
+    die();
+}
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
+$query = "INSERT INTO meldingen (attractie, type, melder, capaciteit, prioriteit, overige_info)
+VALUES(:attractie, :type, :melder, :capaciteit, :prioriteit, :overige_info)";
 
-$query= "INSERT INTO meldingen (attractie , capaciteit, melder, type) VALUES(:attractie, :capaciteit, :melder, :type)";
 //3. Prepare
-
 $statement = $conn->prepare($query);
+
+
+
 //4. Execute
+$test = $statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ":melder" => $melder,
+    ":capaciteit" => $capaciteit,
+    ":prioriteit" => $prioriteit,
+    ":overige_info" => $overig
+]);
 
-$statement->execute([
-    ":attractie"=>$attractie,
-    ":capaciteit"=>$capaciteit,
-    ":melder"=>$melder,
-    ":type"=>$type
 
-    ]);
-
-header("Location:../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");
+//ga terug naar index
+header("Location: ../../../resources/views/meldingen/index.php?msg=Meldingopgeslagen");
+}
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -15,15 +15,15 @@
         <h1>Nieuwe melding</h1>
 
         <form action="<?php echo $base_url; ?>/app/Http/Controllers/meldingenController.php" method="POST">
-
+            <input type="hidden" name="action" value="create">
             <div class="form-group">
                 <label for="attractie">Naam attractie:</label>
                 <input type="text" name="attractie" id="attractie" class="form-input">
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
-                <select name="type"> 
+                <select name="type">
+                    <option value="">kies een type</option>
                     <option value="achtbaan">Achtbaan</option>
                     <option value="draaiend">Draaiend</option>
                     <option value="kinder">Kinder</option>
@@ -41,7 +41,15 @@
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
             </div>
-
+            <div class="form-group">
+                <label for="prioriteit">Prioriteit</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <label for="prioriteit">melding voor prioriteit</label>
+            </div>
+            <div class="overig">
+                <label for="overig">Overige info</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
+            </div>
             <input type="submit" value="Verstuur melding">
 
         </form>
